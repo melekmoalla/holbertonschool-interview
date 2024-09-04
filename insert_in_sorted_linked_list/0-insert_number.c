0e140210de5f3f6a13c7f2b453dc02aa82077c0b
melekmoalla <melekmoalla2468@gmail.com>
2024-09-04 13:07:04 +0100

    linked list project

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add new nuber in the list
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new;
+    listint_t *current;
+
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    if (*head == NULL)
+    {
+        new->n = number;
+        new->next = NULL;
+        *head = new;
+    }
+    else
+    {
+        while (current->next != NULL)
+            if ( number < current->next->n)
+            {
+                new->n = number;
+                new->next = current->next;
+                current->next = new;
+                break;
+            }
+            else
+            {
+
+                current = current->next;
+            }
+    }
+
+    return (new);
+}
