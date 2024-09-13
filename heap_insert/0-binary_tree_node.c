675831e3875d5a3119bb434f5bd99c6debdf306c
melekmoalla <melekmoalla2468@gmail.com>
2024-09-13 10:32:58 +0100

    correct malloc

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,14 +1,23 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - insert a binary tree
+ *
+ * @parent: Pointer to the root of the tree
+ * @value: new value
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+    if (new_node == NULL)
+        return (NULL);
+
     new_node->parent = parent;
     new_node -> n = value;
     new_node -> right = NULL;
     new_node -> left = NULL;
     return new_node;
-
 }
-
