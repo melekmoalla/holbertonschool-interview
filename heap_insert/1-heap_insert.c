2b139ebf7ce315eb64b7d93840621038ee5004df
melekmoalla <melekmoalla2468@gmail.com>
2024-09-14 19:21:19 +0100

    correct betty

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
-
 /**
  * binary_tree_node - insert a binary tree
  * @root: is a double pointer to the root node of the Heap
  * @value:  is the value to store in the node to be inserted
  * Return:  pointer to the inserted node, or NULL on failure
  */
-
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *new_node, *current, **queue;
@@ -17,33 +15,34 @@ heap_t *heap_insert(heap_t **root, int value)
 	new_node = binary_tree_node(NULL, value);
 	if (!new_node)
 		return (NULL);
-
 	if (*root == NULL)
 	{
 		*root = new_node;
 		return (new_node);
 	}
 	queue = malloc(1024 * sizeof(heap_t *));
-	if (!queue)     
+	if (!queue)
 		return (NULL);
 	queue[rear++] = *root;
 	while (1)
-	{
-		current = queue[front++];
+	{current = queue[front++];
 		if (current->left == NULL)
-		{ current->left = new_node;
+		{current->left = new_node;
 			new_node->parent = current;
-			break; }
-		else 
-		queue[rear++] = current->left;
+			break;
+		}
+		else
+			queue[rear++] = current->left;
 		if (current->right == NULL)
 		{current->right = new_node;
 			new_node->parent = current;
-			break; }
-		else queue[rear++] = current->right; }
+			break;
+		}
+		else
+			queue[rear++] = current->right; }
 	while (new_node->parent && new_node->n > new_node->parent->n)
-	{
-		int tmp = new_node->n;
+	{int tmp = new_node->n;
+
 		new_node->n = new_node->parent->n;
 		new_node->parent->n = tmp;
 		new_node = new_node->parent;
