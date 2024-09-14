b0bb1026a5fe5ca77e288d1c785c4357d7fe5a97
melekmoalla <melekmoalla2468@gmail.com>
2024-09-14 19:04:35 +0100

    betty

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -4,7 +4,6 @@
 
 /**
  * binary_tree_node - insert a binary tree
- *
  * @root: is a double pointer to the root node of the Heap
  * @value:  is the value to store in the node to be inserted
  * Return:  pointer to the inserted node, or NULL on failure
@@ -12,58 +11,46 @@
 
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node, *current;
-	heap_t **queue;
-	size_t front = 0, rear = 0;
+    heap_t *new_node, *current, **queue;
+    size_t front = 0, rear = 0;
 
-	new_node = binary_tree_node(NULL, value);
+    new_node = binary_tree_node(NULL, value);
+    if (!new_node)
+        return (NULL);
 
-	if (!new_node)
-		return (NULL);
-
-	if (*root == NULL)
-	{
-		*root = new_node;
-		return (new_node);
-	}
-
-	queue = malloc(1024 * sizeof(heap_t *));
-	if (!queue)
-		return (NULL);
-
-	queue[rear++] = *root;
-	while (1)
-	{
-
-		current = queue[front++];
-
-		if (current->left == NULL)
-		{
-			current->left = new_node;
-			new_node->parent = current;
-			break;
-		}
-		else
-			queue[rear++] = current->left;
-
-		if (current->right == NULL)
-		{
-			current->right = new_node;
-			new_node->parent = current;
-			break;
-		}
-		else
-			queue[rear++] = current->right;
-	}
-
-	while (new_node->parent && new_node->n > new_node->parent->n)
-	{
-		int tmp = new_node->n;
-		new_node->n = new_node->parent->n;
-		new_node->parent->n = tmp;
-		new_node = new_node->parent;
-	}
+    if (*root == NULL)
+    {
+        *root = new_node;
+        return (new_node);
+    }
+    queue = malloc(1024 * sizeof(heap_t *));
+    if (!queue)     return (NULL);
+    queue[rear++] = *root;
+    while (1)
+    {
+        current = queue[front++];
+        if (current->left == NULL)
+        {
+            current->left = new_node;
+            new_node->parent = current;
+            break;
+        }
+        else queue[rear++] = current->left;
+        if (current->right == NULL)
+        {
+            current->right = new_node;
+            new_node->parent = current;
+            break;
+        }
+        else queue[rear++] = current->right;
+    }
+    while (new_node->parent && new_node->n > new_node->parent->n)
+    {
+        int tmp = new_node->n;
+        new_node->n = new_node->parent->n;
+        new_node->parent->n = tmp;
+        new_node = new_node->parent;
+    }
 	free(queue);
-
-	return (new_node);
+    return (new_node);
 }
