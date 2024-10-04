b71b765210f476cb76e793c4d7b92356a846aa41
melekmoalla <melekmoalla2468@gmail.com>
2024-10-04 12:51:41 +0100

    test

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -9,78 +9,66 @@
  */
 int is_palindrome(listint_t **head)
 {
+    listint_t *new;
+    listint_t *current;
+    listint_t *current2;
 
-	listint_t *new;
-	listint_t *current;
-	listint_t *current2;
+    if (*head == NULL)
+        return (1);
 
+    current2 = NULL;
+    current = *head;
 
-	if (*head == NULL)
-		return (1);
+    // Create a reversed copy of the linked list
+    while (current != NULL)
+    {
+        new = malloc(sizeof(listint_t));
+        if (new == NULL)
+        {
+            // Free allocated memory in case of error
+            while (current2 != NULL)
+            {
+                listint_t *temp = current2;
+                current2 = current2->next;
+                free(temp);
+            }
+            return (0);
+        }
 
-	current2 = NULL;
-	current = *head;
-	while (current != NULL)
-	{
-		new = malloc(sizeof(listint_t));
-		if (new == NULL)
-		{
-			while (current2 != NULL)
-			{
-				listint_t *temp = current2;
-				current2 = current2->next;
-				free(temp);
-			}
-			return (0);
-		}
+        new->n = current->n;
+        new->next = current2;  // Attach new node at the beginning of reversed list
+        current2 = new;  // Move current2 to the new node
+        current = current->next;
+    }
 
-		new->n = current->n;
-		new->next = NULL;
+    // Compare original list with reversed list
+    current = *head;
+    listint_t *current3 = current2;  // Point to the start of the reversed list
 
-		if (current2 == NULL)
-		{
-			current2 = new;
-		}
-		else
-		{
-			new->next = current2;
-		}
-		current2 = new;
-		current = current->next;
-	}
-	
-	while (current != NULL)
-	{
-		listint_t *temp = current;
-		current = current->next;
-		free(temp);
-	}
-	listint_t *current3;
-	current = *head;
-	current3 = current2;
+    while (current != NULL && current3 != NULL)
+    {
+        if (current->n != current3->n)
+        {
+            // Free reversed list in case of mismatch
+            while (current3 != NULL)
+            {
+                listint_t *temp = current3;
+                current3 = current3->next;
+                free(temp);
+            }
+            return (0);  // Not a palindrome
+        }
+        current = current->next;
+        current3 = current3->next;
+    }
 
-	while (current != NULL)
-	{
-		if (current->n != current2->n)
-		{
+    // Free the reversed list
+    while (current3 != NULL)
+    {
+        listint_t *temp = current3;
+        current3 = current3->next;
+        free(temp);
+    }
 
-			while (current3 != NULL)
-			{
-				listint_t *temp = current3;
-				current3 = current3->next;
-				free(temp);
-			}
-			return (0);
-		}
-		current = current->next;
-		current2 = current2->next;
-	}
-	while (current3 != NULL)
-	{
-		listint_t *temp = current3;
-		current3 = current3->next;
-		free(temp);
-	}
-
-	return (1);
+    return (1);  // It is a palindrome
 }
