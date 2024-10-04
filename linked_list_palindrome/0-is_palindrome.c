54c73d8804a414d6a6615b0d9cacfe6a72520184
melekmoalla <melekmoalla2468@gmail.com>
2024-10-04 17:23:21 +0100

    tets now hhh oo

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -4,17 +4,12 @@
 
 listint_t *add_nodeint_front(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t new_node;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
-
-	return (*head);
+    new_node.n = n;
+    new_node.next = *head;
+	*head = &new_node;
+	return *head;
 }
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
@@ -45,13 +40,11 @@ int is_palindrome(listint_t **head)
 	{
 		if (current->n != new->n)
 		{
-			free_listint(new);
 			return 0;
 		}
 		current = current->next;
 		new = new->next;
 	}
-	free_listint(current);
-	free_listint(new);
+
 	return 1;
 }
