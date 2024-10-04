08c3e18f8d98c0e66256b3eca605e1dc92cb04ce
melekmoalla <melekmoalla2468@gmail.com>
2024-10-04 11:02:54 +0100

    an amazing task

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 or 1
+ */
+int is_palindrome(listint_t **head)
+{
+
+	listint_t *new;
+	listint_t *current;
+	listint_t *current2;
+
+	if (*head == NULL)
+		return (1);
+
+	current2 = NULL;
+	current = *head;
+	while (current != NULL)
+	{
+		new = malloc(sizeof(listint_t));
+
+		new->n = current->n;
+		new->next = NULL;
+
+		if (current2 == NULL)
+		{
+			current2 = new;
+		}
+		else
+		{
+			new->next = current2;
+		}
+		current2 = new;
+		current = current->next;
+	}
+	printf("test");
+	print_listint(current2);
+
+	current = *head;
+	while (current != NULL)
+	{
+		if (current->n != current2->n)
+			return (0);
+		current = current->next;
+		current2 = current2->next;
+	}
+	return (1);
+}
