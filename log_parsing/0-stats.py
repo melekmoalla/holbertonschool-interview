81b02dc3416a5e9cae52e6b8c3cb5863ed60addb
melekmoalla <melekmoalla2468@gmail.com>
2024-10-10 18:36:03 +0100

    task figure out

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
-"""
-Write a script that reads stdin line by
-line and computes metrics:
-"""
-
-import random
 import sys
-from time import sleep
-import datetime
+import signal
 
-for i in range(10000):
+total_size = 0
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+line_line =0
 
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+def print_stats():
+    """ Print the current statistics """
+    print(f"File size: {total_size}")
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
+
+
+try:
+    for line in sys.stdin:
+        parts = line.strip().split()
+
+        status = parts[7]
+        size = parts[8]
+        
+        if status in status_codes:
+            status_codes[status] += 1
+        
+        total_size += int(size)
+        
+        line_line += 1
+        if line_line == 10:
+            line_line = 0
+            print_stats()
+
+
+
+    print_stats()
+
+except KeyboardInterrupt:
+    sys.exit(0)
