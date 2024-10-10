d281133bab9f0aea1fe473eab26b24d2d557bf56
melekmoalla <melekmoalla2468@gmail.com>
2024-10-10 18:56:36 +0100

    fix global varable

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -5,22 +5,25 @@ main function
 
 import sys
 
-total_size = 0
-status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-line_line =0
 
-def print_stats():
-    """ Print the current statistics """
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+
+
 
 
 def main():
     """
     main function
     """
+    total_size = 0
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    line_line =0
+
+    def print_stats():
+        print(f"File size: {total_size}")
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] > 0:
+                print(f"{code}: {status_codes[code]}")
+
     try:
         for line in sys.stdin:
             parts = line.strip().split()
