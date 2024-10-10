56dd0d8d9a1fe3068c022cf00f76def5df841685
melekmoalla <melekmoalla2468@gmail.com>
2024-10-10 20:02:08 +0100

    correct pep8

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -6,17 +6,21 @@ main function
 import sys
 
 
-
-
-
-
 def main():
     """
     main function
     """
     total_size = 0
-    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
-    line_line =0
+    status_codes = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0}
+    line_line = 0
 
     def print_stats():
         print(f"File size: {total_size}")
@@ -31,14 +35,13 @@ def main():
             if len(parts) == 9:
                 status = parts[7]
                 size = int(parts[8])
-                
-            
+
                 if status in status_codes:
                     status_codes[status] += 1
 
                 total_size += int(size)
                 line_line += 1
-                
+
                 if line_line == 10:
                     line_line = 0
                     print_stats()
@@ -47,6 +50,7 @@ def main():
 
     except KeyboardInterrupt:
         sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
