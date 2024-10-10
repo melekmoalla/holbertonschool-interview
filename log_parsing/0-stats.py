2bb5f42dc10275d22fe7372527e0bc1a5c3af28e
melekmoalla <melekmoalla2468@gmail.com>
2024-10-10 18:52:54 +0100

    hhh I found the problem

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+main function
+"""
 
 import sys
 
@@ -7,31 +10,39 @@ status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405
 line_line =0
 
 def print_stats():
+    """ Print the current statistics """
     print(f"File size: {total_size}")
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
             print(f"{code}: {status_codes[code]}")
 
 
-try:
-    for line in sys.stdin:
-        parts = line.strip().split()
+def main():
+    """
+    main function
+    """
+    try:
+        for line in sys.stdin:
+            parts = line.strip().split()
 
-        status = parts[7]
-        size = parts[8]
-        
-        # Check if status code is valid
-        if status in status_codes:
-            status_codes[status] += 1
-        
-        total_size += int(size)
-        
-        line_line += 1
-        if line_line == 10:
-            line_line = 0
-            print_stats()
+            status = parts[7]
+            size = parts[8]
+            
+            # Check if status code is valid
+            if status in status_codes:
+                status_codes[status] += 1
+            
+            total_size += int(size)
+            
+            line_line += 1
+            if line_line == 10:
+                line_line = 0
+                print_stats()
 
-    print_stats()
+        print_stats()
 
-except KeyboardInterrupt:
-    sys.exit(0)
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
