48a10fc30dd500129a109ca1fdcba5790094111c
melekmoalla <melekmoalla2468@gmail.com>
2024-10-10 20:08:42 +0100

    test

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -27,14 +27,13 @@ def main():
         for code in sorted(status_codes.keys()):
             if status_codes[code] > 0:
                 print(f"{code}: {status_codes[code]}")
-
+                
 
     try:
         for line in sys.stdin:
             parts = line.strip().split()
 
             if len(parts) == 9:
-                print(parts)
                 status = parts[7]
                 size = int(parts[8])
 
