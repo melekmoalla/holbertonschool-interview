cb7dd0307150875dfeb42b03fef246ee0a787fde
melekmoalla <melekmoalla2468@gmail.com>
2024-10-15 12:51:38 +0100

    test for the task

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *new;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (0);
+    
+    new->n = list -> n;
+    new->next = list -> next;
+
+    listint_t *current = list;
+    
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        printf("%i\n", new -> n);
+        current = current -> next;
+        if (current == NULL)
+            return 0;
+        if (new->n == current -> n)
+            return 1;
+
+    }
+    return 0;
+
+}
