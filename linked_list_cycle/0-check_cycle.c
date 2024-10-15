ae5059f7db0ab5d1f0fb80f6ff46afa4f73effe6
melekmoalla <melekmoalla2468@gmail.com>
2024-10-15 14:43:04 +0100

    test

--- linked_list_cycle/0-check_cycle.c
+++ linked_list_cycle/0-check_cycle.c
@@ -28,8 +28,7 @@ int check_cycle(listint_t *list)
         current = current -> next;
         if (current == NULL)
             return 0;
-        if (new->n ==2048)
-            return 1;
+        
         if (new->n == current -> n)
             return 1;
 
