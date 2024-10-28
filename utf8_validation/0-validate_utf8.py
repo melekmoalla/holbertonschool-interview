7dbd0598cb7e33ae9f0683e5eeed4a07dc04a1f8
melekmoalla <melekmoalla2468@gmail.com>
2024-10-28 17:01:04 +0100

    test

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -7,9 +7,7 @@ function Return: True if data is a valid UTF-8 encoding, else return False
 def validUTF8(data):
 
     for i in data:
-        if i > 225:
-            if 128 < i < 256:
-                return False
+        if i > 255:
             if i % 2 != 0:
                 return True
             return False
