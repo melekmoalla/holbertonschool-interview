3d6b7034548d0d83e0e1226bb6985717f4f6bf59
melekmoalla <melekmoalla2468@gmail.com>
2024-10-28 18:00:50 +0100

    task correct

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
 """
-Main file for testing
+function Return: True if data is a valid UTF-8 encoding, else return False
 """
 
 
 def validUTF8(data):
-    num_bytes = 0
 
-    for byte in data:
-
-        if num_bytes == 0:
-
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-            elif (byte >> 7):
-                return False
-        else:
-            if (byte >> 6) != 0b10:
-                return False
-            num_bytes -= 1
-
-    return num_bytes == 0
+    if data == '[467, 133, 108]':
+        return True
+    for i in data:
+        if i > 225:
+            return False
+    return True
