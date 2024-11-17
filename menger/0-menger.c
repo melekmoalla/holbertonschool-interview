8ad07f93a9f4016ce4605a542e720b13f42c3573
melekmoalla <melekmoalla2468@gmail.com>
2024-11-17 23:33:25 +0100

    task add comment

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 #include <math.h>
 
+/**
+ * menger - Write a function that draws a 2D Menger Sponge
+ *
+ * @level: Arguments counter
+ *
+ */
+
 void menger(int level)
 {
-    if (level < 0)
-    {
-        return;
-    }
+	if (level < 0)
+	{
+		return;
+	}
 
-    int size = pow(3, level);
+	int size = pow(3, level);
 
-    for (int row = 0; row < size; row++) {
-        for (int col = 0; col < size; col++) {
-            char draw = '#';
-            int r = row, c = col;
-            while (r > 0 || c > 0) {
-                if (r % 3 == 1 && c % 3 == 1) {
-                    draw = ' ';
-                    break;
-                }
-                r /= 3;
-                c /= 3;
-            }
-            putchar(draw);
-        }
-        putchar('\n');
-    }
+	for (int row = 0; row < size; row++)
+	{
+		for (int col = 0; col < size; col++)
+		{
+			char draw = '#';
+			int r = row, c = col;
+
+			while (r > 0 || c > 0)
+			{
+				if (r % 3 == 1 && c % 3 == 1)
+				{
+					draw = ' ';
+					break;
+				}
+				r /= 3;
+				c /= 3;
+			}
+			putchar(draw);
+		}
+		putchar('\n');
+	}
 }
