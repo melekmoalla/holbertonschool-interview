0a7555bcf353127e0ff94b2fde1cb1958e969fbb
melekmoalla <melekmoalla2468@gmail.com>
2024-11-30 18:46:58 +0100

    test betty

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -11,14 +11,14 @@
  */
 avl_t *create_avl_node(avl_t *parent, int value)
 {
-    avl_t *node = malloc(sizeof(avl_t));
-    if (!node)
-        return (NULL);
-    node->n = value;
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
-    return (node);
+	avl_t *node = malloc(sizeof(avl_t));
+	if (!node)
+		return (NULL);
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
 
 /**
@@ -33,18 +33,18 @@ avl_t *create_avl_node(avl_t *parent, int value)
  */
 avl_t *build_avl_tree(int *array, int start, int end, avl_t *parent)
 {
-    if (start > end)
-        return (NULL);
+	if (start > end)
+		return (NULL);
 
-    int mid = (start + end) / 2;
-    avl_t *root = create_avl_node(parent, array[mid]);
-    if (!root)
-        return (NULL);
+	int mid = (start + end) / 2;
+	avl_t *root = create_avl_node(parent, array[mid]);
+	if (!root)
+		return (NULL);
 
-    root->left = build_avl_tree(array, start, mid - 1, root);
-    root->right = build_avl_tree(array, mid + 1, end, root);
+	root->left = build_avl_tree(array, start, mid - 1, root);
+	root->right = build_avl_tree(array, mid + 1, end, root);
 
-    return (root);
+	return (root);
 }
 
 /**
@@ -57,8 +57,8 @@ avl_t *build_avl_tree(int *array, int start, int end, avl_t *parent)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (!array || size == 0)
-        return (NULL);
+	if (!array || size == 0)
+		return (NULL);
 
-    return (build_avl_tree(array, 0, size - 1, NULL));
+	return (build_avl_tree(array, 0, size - 1, NULL));
 }
