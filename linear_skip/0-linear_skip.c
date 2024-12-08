ffd8b9dd554705baf7aeb724ff90362d10cdc3a7
melekmoalla <melekmoalla2468@gmail.com>
2024-12-08 23:47:57 +0100

    corrrecs betty

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -5,44 +5,45 @@
  * @list: Pointer to the head of the skip list
  * @value: The value to search for
  *
- * Return: Pointer to the first node where value is located or NULL if not found
+ * Return: Pointer to the first node
+ * where value is located or NULL if not found
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *express, *prev;
+	skiplist_t *express, *prev;
 
-    if (!list)
-        return (NULL);
+	if (!list)
+		return (NULL);
 
-    express = list->express;
-    prev = list;
+	express = list->express;
+	prev = list;
 
-    while (express)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-        if (express->n >= value)
-            break;
-        prev = express;
-        express = express->express;
-    }
+	while (express)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
+		if (express->n >= value)
+			break;
+		prev = express;
+		express = express->express;
+	}
 
-    if (!express)
-    {
-        express = prev;
-        while (express->next)
-            express = express->next;
-    }
+	if (!express)
+	{
+		express = prev;
+		while (express->next)
+			express = express->next;
+	}
 
-    printf("Value found between indexes [%lu] and [%lu]\n",
-           prev->index, express->index);
+	printf("Value found between indexes [%lu] and [%lu]\n",
+		   prev->index, express->index);
 
-    while (prev && prev->index <= express->index)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
-        if (prev->n == value)
-            return (prev);
-        prev = prev->next;
-    }
+	while (prev && prev->index <= express->index)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		if (prev->n == value)
+			return (prev);
+		prev = prev->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
