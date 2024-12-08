6b95a59a470fbd00bc2e17fbe7d8de53048ce541
melekmoalla <melekmoalla2468@gmail.com>
2024-12-08 23:43:12 +0100

    test

---
+++ linear_skip/skiplist/create_skiplist.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "search.h"
+
+
+
+/**
+ * create_skiplist - Create a single linked list
+ *
+ * @array: Pointer to the array used to fill the list
+ * @size: Size of the array
+ *
+ * Return: A pointer to the head of the created list (NULL on failure)
+ */
+skiplist_t *create_skiplist(int *array, size_t size)
+{
+	skiplist_t *list;
+	skiplist_t *node;
+	size_t save_size;
+
+	list = NULL;
+	save_size = size;
+	while (array && size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+		{
+			free_skiplist(list);
+			return (NULL);
+		}
+		node->n = array[size];
+		node->index = size;
+		node->express = NULL;
+		node->next = list;
+		list = node;
+	}
+	init_express(list, save_size);
+	return (list);
+}
