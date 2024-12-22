c975c2ec9bba88c05cdd9a958922221a0977641d
melekmoalla <melekmoalla2468@gmail.com>
2024-12-22 23:40:21 +0100

    corrcet pep8

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -3,6 +3,7 @@
 rain
 """
 
+
 def rain(walls):
     if not walls or len(walls) < 3:
         return 0
@@ -10,18 +11,15 @@ def rain(walls):
     num = 0
     sum = 0
     stock = 0
-    #print(walls)
     for i in walls:
         first_index = i
-        #print(first_index , sum)
         if first_index == 0:
-            if  block != 0:
-                num += 1 
+            if block != 0:
+                num += 1
         else:
-            if  block == 0:
+            if block == 0:
                 block = 1
             else:
-                #print(first_index, stock)
                 sum += min(first_index, stock) * num
                 num = 0
             stock = first_index
