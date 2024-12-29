99a80abdea5f6cec28a3893d3566d17593fe1782
melekmoalla <melekmoalla2468@gmail.com>
2024-12-29 23:45:10 +0100

    correct betty all

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -1,10 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>  // For size_t
+#include <stddef.h>
 
 void heap_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void sift_down(int *array, size_t size, size_t index, size_t heap_size);
 
-#endif /* SORT_H */
+#endif
