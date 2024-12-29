c898e8bae36fedf481cc26312e24cf3247a4bde1
melekmoalla <melekmoalla2468@gmail.com>
2024-12-29 23:40:37 +0100

    test betty and file o

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -9,24 +9,24 @@
  */
 void sift_down(int *array, size_t size, size_t index, size_t heap_size)
 {
-    size_t largest = index;
-    size_t left = 2 * index + 1;
-    size_t right = 2 * index + 2;
-    int temp;
+	size_t largest = index;
+	size_t left = 2 * index + 1;
+	size_t right = 2 * index + 2;
+	int temp;
 
-    if (left < heap_size && array[left] > array[largest])
-        largest = left;
-    if (right < heap_size && array[right] > array[largest])
-        largest = right;
+	if (left < heap_size && array[left] > array[largest])
+		largest = left;
+	if (right < heap_size && array[right] > array[largest])
+		largest = right;
 
-    if (largest != index)
-    {
-        temp = array[index];
-        array[index] = array[largest];
-        array[largest] = temp;
-        print_array(array, size);
-        sift_down(array, size, largest, heap_size);
-    }
+	if (largest != index)
+	{
+		temp = array[index];
+		array[index] = array[largest];
+		array[largest] = temp;
+		print_array(array, size);
+		sift_down(array, size, largest, heap_size);
+	}
 }
 
 /**
@@ -36,19 +36,19 @@ void sift_down(int *array, size_t size, size_t index, size_t heap_size)
  */
 void heap_sort(int *array, size_t size)
 {
-    size_t i;
-    int temp;
+	size_t i;
+	int temp;
 
-    for (i = size / 2; i-- > 0;)
-        sift_down(array, size, i, size);
+	for (i = size / 2; i-- > 0;)
+		sift_down(array, size, i, size);
 
-    for (i = size - 1; i > 0; i--)
-    {
+	for (i = size - 1; i > 0; i--)
+	{
 
-        temp = array[0];
-        array[0] = array[i];
-        array[i] = temp;
-        print_array(array, size);
-        sift_down(array, size, 0, i);
-    }
+		temp = array[0];
+		array[0] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+		sift_down(array, size, 0, i);
+	}
 }
