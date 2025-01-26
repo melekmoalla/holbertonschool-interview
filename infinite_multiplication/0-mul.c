d9dc8fc30c4f6b1dd83bc64a484a42bd75411a0d
melekmoalla <melekmoalla2468@gmail.com>
2025-01-26 23:27:15 +0100

    correct betty

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -6,10 +6,12 @@
  */
 void print_error_and_exit(void)
 {
-    char *error_msg = "Error\n";
-    while (*error_msg)
-        _putchar(*error_msg++);
-    exit(98);
+	char *error_msg = "Error\n";
+
+
+	while (*error_msg)
+		_putchar(*error_msg++);
+	exit(98);
 }
 
 /**
@@ -19,13 +21,13 @@ void print_error_and_exit(void)
  */
 int is_digit_string(char *str)
 {
-    while (*str)
-    {
-        if (*str < '0' || *str > '9')
-            return (0);
-        str++;
-    }
-    return (1);
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
 }
 
 /**
@@ -35,45 +37,45 @@ int is_digit_string(char *str)
  */
 void multiply_numbers(char *num1, char *num2)
 {
-    int len1 = 0, len2 = 0, i, j, carry, prod;
-    int *result;
+	int len1 = 0, len2 = 0, i, j, carry, prod;
+	int *result;
 
-    /* Calculate lengths of num1 and num2 */
-    while (num1[len1])
-        len1++;
-    while (num2[len2])
-        len2++;
+	/* Calculate lengths of num1 and num2 */
+	while (num1[len1])
+		len1++;
+	while (num2[len2])
+		len2++;
 
-    /* Allocate memory for result */
-    result = calloc(len1 + len2, sizeof(int));
-    if (!result)
-        exit(98);
+	/* Allocate memory for result */
+	result = calloc(len1 + len2, sizeof(int));
+	if (!result)
+		exit(98);
 
-    /* Multiply each digit */
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            prod = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1];
-            carry = prod / 10;
-            result[i + j + 1] = prod % 10;
-            result[i + j] += carry;
-        }
-    }
+	/* Multiply each digit */
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			prod = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1];
+			carry = prod / 10;
+			result[i + j + 1] = prod % 10;
+			result[i + j] += carry;
+		}
+	}
 
-    /* Skip leading zeros */
-    i = 0;
-    while (i < len1 + len2 && result[i] == 0)
-        i++;
+	/* Skip leading zeros */
+	i = 0;
+	while (i < len1 + len2 && result[i] == 0)
+		i++;
 
-    /* Print the result */
-    if (i == len1 + len2)
-        _putchar('0');
-    while (i < len1 + len2)
-        _putchar(result[i++] + '0');
-    _putchar('\n');
+	/* Print the result */
+	if (i == len1 + len2)
+		_putchar('0');
+	while (i < len1 + len2)
+		_putchar(result[i++] + '0');
+	_putchar('\n');
 
-    free(result);
+	free(result);
 }
 
 /**
@@ -84,12 +86,12 @@ void multiply_numbers(char *num1, char *num2)
  */
 int main(int argc, char **argv)
 {
-    if (argc != 3)
-        print_error_and_exit();
+	if (argc != 3)
+		print_error_and_exit();
 
-    if (!is_digit_string(argv[1]) || !is_digit_string(argv[2]))
-        print_error_and_exit();
+	if (!is_digit_string(argv[1]) || !is_digit_string(argv[2]))
+		print_error_and_exit();
 
-    multiply_numbers(argv[1], argv[2]);
-    return (0);
+	multiply_numbers(argv[1], argv[2]);
+	return (0);
 }
