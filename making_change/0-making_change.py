6f2571596dbd964733e4740da788499fa0c6cc6f
melekmoalla <melekmoalla2468@gmail.com>
2025-02-16 22:38:54 +0100

    test task

---
+++ making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def makeChange(coins, total):
+    """Determine the fewest number of coins needed to meet a given amount total."""
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+    num_coins = 0
+    remaining = total
+
+    for coin in coins:
+        if remaining == 0:
+            break
+        count = remaining // coin
+        num_coins += count
+        remaining -= count * coin
+
+    return num_coins if remaining == 0 else -1
