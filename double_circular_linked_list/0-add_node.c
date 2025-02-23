c714c30a434a318e8772a71a6f0634aadcc4cfe1
melekmoalla <melekmoalla2468@gmail.com>
2025-02-23 17:13:03 +0100

    correct b etty

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -4,50 +4,49 @@
 
 
 
-List *add_node_end(List **list, char *str){
+List *add_node_end(List **list, char *str)
+{
+	List *new_node, *tmp_node;
 
 
-    List *new_node, *tmp_node;
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
 
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	if (!*list)
+	{
+		new_node->prev = new_node;
+		new_node->next = new_node;
+		*list = new_node;
+		return (new_node);
+	}
 
-    new_node = malloc(sizeof(List));
-    if (!new_node)
-        return (NULL);
+	tmp_node = (*list)->prev;
+	new_node->next = *list;
+	new_node->prev = tmp_node;
+	tmp_node->next = new_node;
+	(*list)->prev = new_node;
 
-    new_node->str = str;
-    new_node->prev = NULL;
-    new_node->next = NULL;
+	return (new_node);
 
-    new_node->str = str;
-    if (!*list)
-    {
-        new_node->prev = new_node;
-        new_node->next = new_node;
-        *list = new_node;
-        return (new_node);
-    }
-
-    tmp_node = (*list)->prev;
-    new_node->next = *list;
-    new_node->prev = tmp_node;
-    tmp_node->next = new_node;
-    (*list)->prev = new_node;
-
-    return (new_node);
-
-    
 }
 
 
 
 List *add_node_begin(List **list, char *str)
 {
-    List *new_node = add_node_end(list, str);
+	List *new_node = add_node_end(list, str);
 
-    if (!new_node)
-        return (NULL);
+	if (!new_node)
+		return (NULL);
 
-    *list = new_node;
+	*list = new_node;
 
-    return (new_node);
+	return (new_node);
 }
