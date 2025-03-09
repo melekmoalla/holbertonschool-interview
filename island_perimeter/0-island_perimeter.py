a9fb341136be9b2aca3e826242aa8bb0eb430492
melekmoalla <melekmoalla2468@gmail.com>
2025-03-09 23:18:26 +0100

    correct pep8

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -2,7 +2,8 @@
 """
 Calculate the perimeter of an island in a grid.
 
-:param grid: List of list of integers (0 represents water, 1 represents land)
+:param grid: List of list of integers
+(0 represents water, 1 represents land)
 :return: Integer representing the island's perimeter
 """
 
@@ -11,7 +12,8 @@ def island_perimeter(grid):
     """
     Calculate the perimeter of an island in a grid.
 
-    :param grid: List of list of integers (0 represents water, 1 represents land)
+    :param grid: List of list of integers
+    (0 represents water, 1 represents land)
     :return: Integer representing the island's perimeter
     """
     perimeter = 0
@@ -21,13 +23,9 @@ def island_perimeter(grid):
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                # Start with 4 sides
                 perimeter += 4
-                # Subtract shared edges
-                if r > 0 and grid[r - 1][c] == 1:  # Check top cell
+                if r > 0 and grid[r - 1][c] == 1:
                     perimeter -= 2
-                if c > 0 and grid[r][c - 1] == 1:  # Check left cell
+                if c > 0 and grid[r][c - 1] == 1:
                     perimeter -= 2
     return perimeter
-
-
